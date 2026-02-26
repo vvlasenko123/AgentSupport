acd3eedfa16cd916399f94dd825f110715b43585
vvlasenko <vvlasenko@artsofte.ru>
2026-02-26 05:45:03 +0500

    init backend

---
+++ backend/Libs/Infrastructure/Infrastructure.Logging/SerilogStartUp.cs
@@ -0,0 +1,33 @@
+﻿using Infrastructure.Logging.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Infrastructure.Logging;
+
+/// <summary>
+/// Добавелние настроек Serilog
+/// </summary>
+public static class SerilogStartUp
+{
+    /// <summary>
+    /// Поключение Serilog
+    /// </summary>
+    public static IHostBuilder UseInfraSerilog(this IHostBuilder builder)
+    {
+        // bootstrap для раннего лога ошибок, потом используем финальный логгер
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateBootstrapLogger();
+
+        builder.UseSerilog(configureLogger: (context, provider, config) =>
+        {
+            // финальный логгер
+            // todo добавить трейсы запросов, чтобы могли различать
+            // var traceId = Activity.Current?.TraceId.ToString();
+            // var spanId = Activity.Current?.SpanId.ToString();
+            config.Configure(context, provider);
+        });
+        
+        return builder;
+    }
+}
